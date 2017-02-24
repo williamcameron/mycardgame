ac9b8d6818bd7724ab6ba79746b8fbc3fb67d847
William Cameron <wp.cameron@gmail.com>
2017-02-24 22:59:36 +0000

    Tapping and untapping cards

--- src/Card.php
+++ src/Card.php
@@ -2,8 +2,31 @@
 	namespace William;
 
 	class Card {
+		
+		private $tapped;
+		private $name;
+		
+		public function __construct($name=""){
+			$this->name = $name;
+			$this->tapped = false;
+		}
+		
 		public function isLand(){
 			return true;
 		}
-
+		
+		public function isCreature(){
+			return false;
+		}
+		
+		public function tap(){
+			$this->tapped = true;
+		}
+		
+		public function tapped(){
+			return $this->tapped;
+		}
+		public function unTap(){
+			$this->tapped = false;
+		}
 	}
--- tests/CardTest.php
+++ tests/CardTest.php
@@ -11,4 +11,31 @@
 			$card = new William\Card("Island");
 			$this->assertTrue($card->isLand());
 		}
+		
+		/** @test */
+		public function basic_land_card_is_not_a_creature(){
+			$card = new William\Card("Island");
+			$this->assertFalse($card->isCreature());
+		}
+		
+		/** @test */
+		public function not_tapped_by_default(){
+			$card = new William\Card;
+			$this->assertFalse($card->tapped());
+		}
+	
+		/** @test */
+		public function tapping_taps_card(){
+			$card = new William\Card;
+			$card->tap();
+			$this->assertTrue($card->tapped());
+		}
+		
+		/** @test */
+		public function tapped_card_can_be_untapped(){
+			$card = new William\Card;
+			$card->tap();
+			$card->unTap();
+			$this->assertFalse($card->tapped());
+		}
 	}
