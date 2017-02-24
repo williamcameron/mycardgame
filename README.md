71488837e9a8846046ba95a5cc5d8f7e90ef8680
William Cameron <wp.cameron@gmail.com>
2017-02-24 19:17:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Beginnings of TDD Card Game
---
+++ tests/gameTest.php
@@ -0,0 +1,13 @@
+<?php
+	class gameTest extends PHPUnit_Framework_TestCase {
+		/** @test */
+		public function php_unit_works(){
+			$this->assertTrue(true);
+		}
+		
+		/** @test */
+		public function game_is_a_game_object(){
+			$game = new William\Game;
+			$this->assertInstanceOf("William\Game", $game);
+		}
+	}
